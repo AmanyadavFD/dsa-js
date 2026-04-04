67b271cefaf39ceeee6d0847f59ff56b941b6b2b
Aman Yadav <amanyadav@Amans-MacBook-Air.local>
2026-04-05 01:30:32 +0530

    solved the single number in the array

---
+++ Arrays - Easy-Medium/single_number.js
@@ -0,0 +1,17 @@
+function singleNumber(nums){
+ let hash = {};
+
+ for(let i =0; i<nums.length;i++){
+    if(!hash[nums[i]]){
+        hash[nums[i]]= 1;
+    }else{
+        hash[nums[i]]++;
+    }
+ }
+ for(let i = 0; i <nums.length;i++){
+    if(hash[nums[i]]===1){
+        return nums[i];
+    }
+ }
+}
+console.log(singleNumber([3,1,5,4,1,5,3]))
